572613e799f4b47602079750a43b68fba3bf665d
1998arunrekha <arunar16061998@gmail.com>
2022-09-19 17:42:12 +0530

    second

--- main.tf
+++ main.tf
@@ -32,8 +32,8 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "example-machine"
+resource "azurerm_windows_virtual_machine" "vm0111" {
+  name                = "newvm11"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_F2"
