78e4589a7838a02017198f9d39f7e075bc4267bf
1998arunrekha <arunar16061998@gmail.com>
2022-09-19 17:38:25 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,57 @@
+# creating resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "vm1"
+  location = "West Europe"
+}
+
+# creating virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet0011"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# creating subnet
+resource "azurerm_subnet" "sb" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet0011.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic11"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminarun"
+  admin_password      = "Aruna@123456"
+  /*network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]*/
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
